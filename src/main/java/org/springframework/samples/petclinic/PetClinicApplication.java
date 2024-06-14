cab25bf311cfdf14f4f26a4571973370d731d8cd
alireza-akbarian <alireza.akbarian@quant.network>
2024-06-14 14:20:50 +0100

    add badsmell codes

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -18,17 +18,12 @@ package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportRuntimeHints;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 /**
  * PetClinic Spring Boot Application.
  *
  * @author Dave Syer
- *
  */
 @SpringBootApplication
 @ImportRuntimeHints(PetClinicRuntimeHints.class)
@@ -36,6 +31,52 @@ public class PetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
+		processData();
+		processMoreData();
+	}
+
+	// Long method with duplicated code and magic numbers
+	public static void processData() {
+		int[] data = {1, 2, 3, 4, 5};
+		int sum = 0;
+		for (int i = 0; i < data.length; i++) {
+			sum += data[i];
+		}
+		double average = sum / 5.0; // Magic number 5.0
+		System.out.println("Sum: " + sum);
+		System.out.println("Average: " + average);
+
+		// Duplicated code for processing another set of data
+		int[] moreData = {6, 7, 8, 9, 10};
+		int moreSum = 0;
+		for (int i = 0; i < moreData.length; i++) {
+			moreSum += moreData[i];
+		}
+		double moreAverage = moreSum / 5.0; // Magic number 5.0
+		System.out.println("Sum: " + moreSum);
+		System.out.println("Average: " + moreAverage);
+	}
+
+	// Another long method with different data but same processing logic
+	public static void processMoreData() {
+		int[] data = {11, 12, 13, 14, 15};
+		int sum = 0;
+		for (int i = 0; i < data.length; i++) {
+			sum += data[i];
+		}
+		double average = sum / 5.0; // Magic number 5.0
+		System.out.println("Sum: " + sum);
+		System.out.println("Average: " + average);
+
+		// Duplicated code for processing another set of data
+		int[] moreData = {16, 17, 18, 19, 20};
+		int moreSum = 0;
+		for (int i = 0; i < moreData.length; i++) {
+			moreSum += moreData[i];
+		}
+		double moreAverage = moreSum / 5.0; // Magic number 5.0
+		System.out.println("Sum: " + moreSum);
+		System.out.println("Average: " + moreAverage);
 	}
 
 }
